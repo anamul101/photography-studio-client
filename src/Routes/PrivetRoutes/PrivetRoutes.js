7b2ddd1150c87a5bbbdea4630706d884bdad9ea4
anamul101 <rjanamul01871@gmail.com>
2022-11-08 15:20:45 +0000

    Privet Routers

--- src/Routes/PrivetRoutes/PrivetRoutes.js
+++ src/Routes/PrivetRoutes/PrivetRoutes.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Navigate, useLocation } from 'react-router-dom';
+import { AuthContext } from '../../Contexts/AuthProvider';
 
-const PrivetRoutes = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+
+const PrivetRoutes = ({children}) => {
+    const {user,loader} = useContext(AuthContext);
+    const location = useLocation()
+    if(loader){
+        return <div className='text-center text-green-600 text-2xl font-bold my-40'>Loading...</div>
+    }
+    if(user){
+        return children;
+    }
+    return <Navigate to='/lognin' state={{from:location}} replace ></Navigate>
 };
 
 export default PrivetRoutes;
