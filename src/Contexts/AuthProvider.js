64b1404b29f31a4be0d27239266ce4ae0d1a4e3c
anamul101 <rjanamul01871@gmail.com>
2022-11-08 15:15:35 +0000

    Athentication

---
+++ src/Contexts/AuthProvider.js
@@ -0,0 +1,78 @@
+import React, { createContext, useEffect, useState } from 'react';
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged, sendEmailVerification, sendPasswordResetEmail, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth';
+import app from '../Firebase/Firebase.config';
+
+
+
+ export const AuthContext = createContext();
+ const auth = getAuth(app);
+
+const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const [loader, setLoader] = useState(true);
+
+    // google sign in 
+    const authSignInGoogle = (provider)=>{
+        setLoader(true)
+        return signInWithPopup(auth,provider);
+    }
+    // GitHub sign in 
+    const authSignInGithub = (provider)=>{
+        setLoader(true)
+        return signInWithPopup(auth, provider);
+    }
+    // form with register
+    const createUser = (email,password)=>{
+        setLoader(true)
+        return createUserWithEmailAndPassword(auth,email,password);
+    }
+    // lognIn with password
+    const authLognIn =(email,password)=>{
+        setLoader(true)
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    // ubdate Profile
+    const authUpdate = (profile)=>{
+        setLoader(true);
+        return updateProfile(auth.currentUser,profile);
+    }
+    // Reset password
+    const forgetPassword=(email)=>{
+        return sendPasswordResetEmail(auth, email);
+    }
+    // logOut
+    const LogOut = ()=>{
+        setLoader(true);
+        return signOut(auth);
+    } 
+
+    useEffect(()=>{
+        const unSubscibe = onAuthStateChanged(auth,(currentUser)=>{
+            setUser(currentUser);
+            setLoader(false)
+        })
+        return ()=>{
+            unSubscibe();
+        }
+    },[])
+    const authInfo = {
+        user,
+        loader,
+        authSignInGoogle,
+        LogOut,
+        createUser,
+        authLognIn,
+        authUpdate,
+        setLoader,
+        authSignInGithub,
+        setUser,
+        forgetPassword
+    };
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
